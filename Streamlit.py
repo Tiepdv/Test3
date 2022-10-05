aaaf967146a36a7e4f4a960b87bc9ad005874d5b
Tiepdv <75802102+Tiepdv@users.noreply.github.com>
2022-10-05 11:26:19 +0200

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,44 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
-# pip install streamlit
-# Check with with pyarrow 3.0.0 or similar stuffs which is removed or updated.....to avoid conflict
-
-
-# In[1]:
-
+# streamlit_app.py
 
 import streamlit as st
-import pandas as pd
-import numpy as np
+from google.oauth2 import service_account
 from google.cloud import bigquery
-client = bigquery.Client()
 
-sql = """
-    SELECT name, SUM(number) as count
-    FROM `bigquery-public-data.usa_names.usa_1910_current`
-    GROUP BY name
-    ORDER BY count DESC
-    LIMIT 10
-"""
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+
+sql = "SELECT AdvertisingSystem, PubAccId FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson` LIMIT 10"
 
 df = client.query(sql).to_dataframe()
 
-st.write("""# My first app 
-Hello *world!*""")
-
-df = pd.DataFrame(
-   np.random.randn(10, 5),
-   columns=('col %d' % i for i in range(5)))
-
+# Print results.
 st.table(df)
-
-
-# In[ ]:
-
-
-
-
