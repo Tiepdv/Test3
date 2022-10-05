6181c8310154a27b351b32dbc78a3c758cb421ab
Tiepdv <75802102+Tiepdv@users.noreply.github.com>
2022-10-05 17:19:04 +0200

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -5,15 +5,26 @@ import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
+st.write("""# IAB dataset
+---------------""")
+
+option = st.selectbox(
+    'Please choose the dataset',
+    ('WEB', 'APP'))
+
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
 client = bigquery.Client(credentials=credentials)
 
-query="SELECT AdvertisingSystem, PubAccId FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson` LIMIT 10000"
+if option=='WEB':
+	query="SELECT * FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains` limit 10"
+
+elif option=='APP':
+	query="SELECT * FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains` limit 10"
+
+
 query_job = client.query(query)
-
 df=client.query(query).to_dataframe()
-
 st.table(df)
