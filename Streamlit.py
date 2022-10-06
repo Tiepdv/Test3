813ffc007f05ec95fac3b4e6da872cb6068d01ea
Tiepdv <75802102+Tiepdv@users.noreply.github.com>
2022-10-06 13:57:19 +0200

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -28,16 +28,15 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 client = bigquery.Client(credentials=credentials)
 
-if choice=="A":
-	if option=='WEB':
-		query="SELECT * FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains` limit 1000"
-
-	elif option=='APP':
-		query="SELECT * FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains` limit 1000"
-
-
+if choice=="WEB":
+	query="SELECT * FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains` limit 1000"
 	query_job = client.query(query)
 	df=client.query(query).to_dataframe()
-	st.table(df)
-elif choice=="B":
+	st.dataframe(df)
+elif choice=="APP":
+	query="SELECT * FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains` limit 1000"
+	query_job = client.query(query)
+	df=client.query(query).to_dataframe()
+	st.dataframe(df)
+
 	st.write('Hello')
