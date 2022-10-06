570ceaf4c43baf598ca1a1d35d19ef9fd21a0e50
Tiepdv <75802102+Tiepdv@users.noreply.github.com>
2022-10-06 14:12:45 +0200

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -32,10 +32,10 @@ if choice=="WEB":
 	query="SELECT * FROM `showheroes-bi.bi.bi_adstxt_join_sellerjson_with_count_domains` limit 1000"
 	query_job = client.query(query)
 	df=client.query(query).to_dataframe()
-	st.dataframe(df, width=None, height=None)
+	st.dataframe(df, width=None, height=10000)
 elif choice=="APP":
 	query="SELECT * FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains` limit 1000"
 	query_job = client.query(query)
 	df=client.query(query).to_dataframe()
-	st.dataframe(df,height=1000)
+	st.dataframe(df,height=10000)
 
